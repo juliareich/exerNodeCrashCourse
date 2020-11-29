02e4e9cb61bd18489520db60270182f6acd44e23
juliareich <jareich23@gmail.com>
2020-11-29 11:36:24 -0700

    completed node crash course

---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+//Platform
+console.log(os.platform());
+
+//CPU arch
+console.log(os.arch());
+
+//CPU core info
+console.log(os.cpus());
+
+//Free momory
+console.log(os.freemem());
+
+//Total momory
+console.log(os.totalmem());
+
+//Home dir
+console.log(os.homedir());
+
+//uptime
+console.log(os.uptime());
